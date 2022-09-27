84c127794afad6656e5d729c12cac9eac331aac7
kaito0902 <66310212+kaito0902@users.noreply.github.com>
2022-09-28 06:04:09 +0900

    HTTP通信による画像の取得

--- app/src/main/java/com/sghh/httpapp/MainActivity.java
+++ app/src/main/java/com/sghh/httpapp/MainActivity.java
@@ -1,14 +1,66 @@
 package com.sghh.httpapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.os.HandlerCompat;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etitText;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etitText = findViewById(R.id.uri);
+        imageView = findViewById(R.id.img);
+        Button downloadButton = findViewById(R.id.download);
+        downloadButton.setOnClickListener(v -> {
+            String stringUrl = etitText.getText().toString();
+            dowloadImage(stringUrl);
+        });
+    }
+
+    private void dowloadImage(String urlSt){
+        // Singleの別スレッドを立ち上げる
+        Executors.newSingleThreadExecutor().execute(() -> {
+            try {
+                URL url = new URL(urlSt);
+                HttpURLConnection urlCon =  (HttpURLConnection) url.openConnection();
+
+                // タイムアウト設定
+                urlCon.setReadTimeout(10000);
+                urlCon.setConnectTimeout(20000);
+
+                // リクエストメソッド
+                urlCon.setRequestMethod("GET");
+
+                // リダイレクトを自動で許可しない設定
+                urlCon.setInstanceFollowRedirects(false);
+
+                InputStream is = urlCon.getInputStream();
+                Bitmap bmp = BitmapFactory.decodeStream(is);
+
+                // 別スレッド内での処理を管理し実行する
+                HandlerCompat.createAsync(getMainLooper()).post(() ->
+                        // Mainスレッドに渡す
+                        imageView.setImageBitmap(bmp)
+                );
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
